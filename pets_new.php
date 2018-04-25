b756dd78d3dc7fd18880f904d472641de4b54b61
Steef <stefanodegenkamp@gmail.com>
2018-04-25 22:07:41 +0200

    Pets new page created

---
+++ pets_new.php
@@ -0,0 +1,9 @@
+<?php require 'layout/header.php'; ?>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-6">
+                <h1>Add your pet!</h1>
+            </div>
+        </div>
+    </div>
+<?php require 'layout/footer.php'; ?>
